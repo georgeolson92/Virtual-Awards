907139b6272f31c8f5776cf164a83858a33fa713
George Olson <georgeolson92@gmail.com>
2022-02-21 20:19:19 +0100

    Move javascript into separate file

---
+++ js/scripts.js
@@ -0,0 +1,195 @@
+
+  var tag = document.createElement('script');
+  tag.id = 'iframe-demo';
+  tag.src = 'https://www.youtube.com/iframe_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('existing-iframe-example', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+    });
+  }
+  function onPlayerReady(event) {
+    document.getElementById('existing-iframe-example').style.borderColor = '#FF6D00';
+  }
+  
+  
+  function onPlayerStateChange(e)
+{
+  if (e.data == 1 && ytSeconds > 0) {
+    e.target.seekTo(ytSeconds);
+    ytSeconds = 0;
+  }
+  
+  if (e.data === 0) {
+  	console.log("ended!");
+    endVideo();
+  }
+}
+
+function seekTo(seconds)
+{
+  if (player.getPlayerState() == 1) {
+    player.seekTo(seconds);
+  }
+  else {
+    ytSeconds = seconds;
+    player.playVideo();
+  }
+}
+
+
+function pauseVideo()
+{
+	player.pauseVideo();
+}
+
+function endVideo()
+{
+	
+    			$('.ytplayer').fadeOut( "1000", function() {
+    			
+    			});
+    			
+    			$('.ytplayer-info').fadeOut( "1000", function() {
+    			
+    			});
+    			
+    			$('.curtainanimation1').each(function() {
+        			
+            			$(this).animate({
+                			left: '0%',
+            			}, 800 );
+            		
+    			});
+            		
+            	$('.curtainanimation2').each(function() {
+            			$(this).animate({
+                			right: '0%',
+            			}, 800 );
+    			});
+    			
+    			
+    			$('.thanks').fadeIn( "1000", function() {
+    			
+    			});
+    			
+    			$('.sharethis-inline-share-buttons .st-btn').fadeIn( "1000", function() {
+    			
+    			});
+    			
+    			$('.sharethis-inline-share-buttons .st-label').fadeIn( "1000", function() {
+    			
+    			});
+    			
+    			
+    			
+}
+    	$(document).ready(function() {
+    		 $('.magic').delay(800).queue(function (next) { 
+  					$(this).css("opacity", "1");
+  					$('#btnOpen').css("opacity","1");
+  					next(); 
+  				});
+    		$('.thanks').hide();
+    		$('.player').hide();
+    		$('.player-info').hide();
+    		$('.ytplayer-info').hide();
+    		$( "#categoryLink" ).addClass("ytplayer-down");
+    		
+    	
+    		$("#btnOpen").click(function(){
+    			$('.magic').css("opacity", "0");
+    			
+    			$('#btnOpen').css("opacity", "0");
+    			
+    			
+    			$('.spot').hide();
+    			
+    			
+    			$('.ytplayer').fadeIn( "1000", function() {
+    			
+    			});
+    			
+    			$('.ytplayer-info').fadeIn( "1000", function() {
+    			
+    			});
+    			
+    			
+    		
+    		
+        		$('.curtainanimation1').each(function() {
+        			if ($(this).offset().left < 0) {
+            			$(this).css("left", "45%");
+        			} else if ($(this).offset().left > $('#container').width()) {
+            			$(this).animate({
+                			left: '10%',
+            			}, 800 );
+        			} else {
+            			$(this).animate({
+                			left: '-45%',
+            		}, 800 );
+            	}
+    			});
+            		
+            	$('.curtainanimation2').each(function() {
+        			if ($(this).offset().right < 0) {
+            			$(this).css("right", "45%");
+        			} else if ($(this).offset().right > $('#container').width()) {
+            			$(this).animate({
+                			right: '10%',
+            			}, 800 );
+        			} else {
+            			$(this).animate({
+                			right: '-45%',
+            		}, 800 );
+        		}
+    			});
+    			
+    			
+    			$('.magic').delay(1000).queue(function (next) { 
+    				$('.magic').hide();
+    				$('#btnOpen').hide();
+    			});
+    		
+    		}); 
+    		
+    		
+    		$('.card a').click(function() {
+    			$( "#categoryLink" ).removeClass("ytplayer-up");
+    				$( "#categoryLink" ).addClass("ytplayer-down");
+    				$( ".ytplayer-info" ).removeClass("ytplayerinfo-up");
+    				$( ".ytplayer-info" ).addClass("ytplayerinfo-down");
+    		});
+    		
+    		
+    		$("#categoryLink").click(function(){
+    			if ($("#categoryLink").hasClass("ytplayer-down")) {
+    				$( "#categoryLink" ).removeClass("ytplayer-down");
+    				$( "#categoryLink" ).addClass("ytplayer-up");
+    				$( ".ytplayer-info" ).removeClass("ytplayerinfo-down");
+    				$( ".ytplayer-info" ).addClass("ytplayerinfo-up");
+    			} else {
+    				$( "#categoryLink" ).removeClass("ytplayer-up");
+    				$( "#categoryLink" ).addClass("ytplayer-down");
+    				$( ".ytplayer-info" ).removeClass("ytplayerinfo-up");
+    				$( ".ytplayer-info" ).addClass("ytplayerinfo-down");
+    			}
+    		});
+    		
+    		$(".ytplayer-up").click(function(){
+    		console.log("hi");
+    			$( "#categoryLink" ).removeClass("ytplayer-up");
+    			$( "#categoryLink" ).addClass("ytplayer-down");
+    			
+    			$( ".ytplayer-info" ).removeClass("ytplayerinfo-up");
+    			$( ".ytplayer-info" ).addClass("ytplayerinfo-down");
+    		});
+    		
+		});
+    
