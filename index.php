5b33947f7664386b92dbb820f0f4acd49804e98f
George Olson <georgeolson92@gmail.com>
2022-03-07 15:44:58 -0800

    Formatting of PHP template files

--- index.php
+++ index.php
@@ -1,69 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
   <head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="George Olson, 2020-2022" />
     <title>SALPies 2020: Ta-Da!</title>
 
     <!-- Font Awesome -->
-    <script
-      src="https://kit.fontawesome.com/715c69a354.js"
-      crossorigin="anonymous"
-    ></script>
+    <script src="https://kit.fontawesome.com/715c69a354.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.typekit.net/rvs6ruk.css" />
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet" />
 
     <!-- Favicons -->
-    <link
-      rel="apple-touch-icon"
-      href="img/favicons/apple-touch-icon.png"
-      sizes="180x180"
-    />
-    <link
-      rel="icon"
-      href="img/favicons/favicon-32x32.png"
-      sizes="32x32"
-      type="image/png"
-    />
-    <link
-      rel="icon"
-      href="img/favicons/favicon-16x16.png"
-      sizes="16x16"
-      type="image/png"
-    />
+    <link rel="apple-touch-icon" href="img/favicons/apple-touch-icon.png" sizes="180x180" />
+    <link rel="icon" href="img/favicons/favicon-32x32.png" sizes="32x32" type="image/png" />
+    <link rel="icon" href="img/favicons/favicon-16x16.png" sizes="16x16" type="image/png" />
     <link rel="icon" href="img/favicons/favicon.ico" />
 
     <!-- Styles -->
     <link href="css/style.css" rel="stylesheet" />
 
     <!-- Scripts -->
-    <script
-      src="https://code.jquery.com/jquery-3.5.1.min.js"
-      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-      crossorigin="anonymous"
-    ></script>
-    <script
-      type="text/javascript"
-      src="https://platform-api.sharethis.com/js/sharethis.js#property=5ecd6022cbdf4100127d4004&product=inline-share-buttons"
-      async="async"
-    ></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=5ecd6022cbdf4100127d4004&product=inline-share-buttons" async="async"></script>
     <script type="text/javascript" src="js/scripts.js" async="async"></script>
   </head>
 
   <!-- Body -->
+
   <body class="text-center">
     <main role="main" class="inner cover">
       <div class="wrapper">
         <div class="curtain-left curtainanimation-left"></div>
         <div class="curtain-right curtainanimation-right"></div>
-        <div class="spot"><img src="img/spotlight.png" /></div>
+        <div class="spot"><img src="img/spotlight.png" alt="Spotlight image for backdrop"/></div>
 
         <div class="thanks">
           <div class="row">
@@ -71,36 +45,24 @@
               <h2>Thanks for watching!</h2>
             </div>
             <div class="col-lg-12 btn-col">
-              <button
-                type="button"
-                onClick="window.location.reload();"
-                class="btn btn-primary btn-open btn-startagain"
-              >
+              <button type="button" onClick="window.location.reload();" class="btn btn-primary btn-open btn-startagain">
                 Watch Again
               </button>
               <div class="socialmedia">
                 <div class="sharethis-inline-share-buttons"></div>
                 <p>Find us online:</p>
-                <a
-                  href="https://www.facebook.com/studentactivitiesandleadership/"
-                  ><i class="fab fa-facebook-square"></i
-                ></a>
-                <a href="https://twitter.com/salpconnect"
-                  ><i class="fab fa-twitter-square"></i
-                ></a>
-                <a
-                  href="https://www.instagram.com/student_leaders_portland_state/"
-                  ><i class="fab fa-instagram-square"></i
-                ></a>
+                <a href="https://www.facebook.com/studentactivitiesandleadership/"><i class="fab fa-facebook-square"></i></a>
+                <a href="https://twitter.com/salpconnect"><i class="fab fa-twitter-square"></i></a>
+                <a href="https://www.instagram.com/student_leaders_portland_state/"><i class="fab fa-instagram-square"></i></a>
                 <a href="#"><i class="fab fa-youtube-square"></i></a>
               </div>
             </div>
           </div>
         </div>
 
-        <?php include 'templates/magic.php' ;?>
+        <?php include 'templates/magic.php'; ?>
 
-        <?php include 'templates/player.php';?>
+        <?php include 'templates/player.php'; ?>
 
         <div class="row">
           <div class="col-lg-12 btn-col">
@@ -113,9 +75,10 @@
         <div class="ytplayer-info ytplayerinfo-down">
           <div class="curtain1" style="left: -45%"></div>
           <div class="curtain2" style="right: -45%"></div>
-          <?php include 'templates/categories.php';?>
+          <?php include 'templates/categories.php'; ?>
         </div>
       </div>
     </main>
   </body>
+
 </html>
--- templates/magic.php
+++ templates/magic.php
@@ -1,26 +1,26 @@
 <div class="magic">
-            <div class="left-hand">
-			  <img src="img/handleft.png" />
-			</div>
-            <div class="right-hand">
-			  <img src="img/handright.png" />
-			</div>
-            <div class="stars">
-			  <img src="img/stars.gif" />
-			</div>
-            <div class="salpies">
-			  <img src="img/SALPies.png" />
-			</div>
-            <div class="twenty">
-			  <img src="img/2020.png" />
-			</div>
-            <div class="tada">
-			  <img src="img/Ta-Da!.png" />
-			</div>
-            <div class="hat">
-			  <img src="img/hat.png" />
-			</div>
-            <div class="light">
-			  <img src="img/light.png" />
-			</div>
+    <div class="left-hand">
+        <img src="img/handleft.png" alt="left hand in SALPies logo animation"/>
+    </div>
+    <div class="right-hand">
+        <img src="img/handright.png" alt="right hand in SALPies logo animation"/>
+    </div>
+    <div class="stars">
+        <img src="img/stars.gif" alt="sparkly stars in SALPies logo animation"/>
+    </div>
+    <div class="salpies">
+        <img src="img/SALPies.png" alt="SALPies logo"/>
+    </div>
+    <div class="twenty">
+        <img src="img/2020.png" alt="year 2020 for SALPies logo"/>
+    </div>
+    <div class="tada">
+        <img src="img/Ta-Da!.png" alt="Ta Da!"/>
+    </div>
+    <div class="hat">
+        <img src="img/hat.png" alt="hat image for SALPies logo animation"/>
+    </div>
+    <div class="light">
+        <img src="img/light.png" alt="spotlight for SALPies logo animation"/>
+    </div>
 </div>
